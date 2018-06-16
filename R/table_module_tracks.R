9d17a7c89911aedfc5524b9ca26b85657bf8e010
Brooke Anderson <brooke.anderson@colostate.edu>
2018-06-15 18:21:59 -0600

    Expand module 'tracks' table

--- R/table_module_tracks.R
+++ R/table_module_tracks.R
@@ -4,12 +4,26 @@ library(kableExtra)
 
 my_table <- tribble(
   ~ ` `, ~ `Student`, ~`PI`, ~ `Research associate`,
-  "\\tabitem Really, really long module name for module 1", "Yes", "No", "No",
-  "\\tabitem Module 2", "Yes", "Yes", "No",
-  "\\tabitem Module 3", "Yes", "No", "Yes",
-  "\\tabitem Module 4", "Yes", "No", "No",
-  "\\tabitem Module 5", "Yes", "No", "No",
-  "\\tabitem Module 6", "Yes", "No", "Yes"
+  "\\tabitem Separating data recording and analysis", "Yes", "No", "No",
+  "\\tabitem Principals and power of structured data formats", "Yes", "Yes", "No",
+  "\\tabitem The 'tidy' data format: an implementation of a structured data format", "Yes", "No", "Yes",
+  "\\tabitem Designing templates for tidy data collection", "Yes", "No", "No",
+  "\\tabitem Example: Creating a template for data collection", "Yes", "No", "No",
+  "\\tabitem Power of using a single 'Project' directory for storing and tracking research project files", "Yes", "No", "Yes",
+  "\\tabitem Creating 'Project' templates", "Yes", "No", "No",
+  "\\tabitem Example: Creating a 'Project' template", "Yes", "Yes", "No",
+  "\\tabitem Harnessing version control for transparent data recording", "Yes", "No", "Yes",
+  "\\tabitem Using git and GitLab to enhance the reproducibility of collaborative research", "Yes", "No", "No",
+ 
+  "\\tabitem Principals and benefits of scripted pre-processing of experimental data", "Yes", "No", "No",
+  "\\tabitem Introduction to R code scripts", "Yes", "Yes", "No",
+  "\\tabitem Simplify scripted pre-processing through R's 'tidyverse' tools", "Yes", "No", "Yes",
+  "\\tabitem Complex data types in experimental data pre-processing", "Yes", "No", "No",
+  "\\tabitem Complex data types in R and Bioconductor", "Yes", "No", "No",
+  "\\tabitem Example: Converting from complex data types to 'tidy' data formats", "Yes", "No", "No",
+  "\\tabitem Introduction to reproducible data pre-processing protocols", "Yes", "Yes", "No",
+  "\\tabitem Introduction to RMarkdown as a tool for creating reproducible data pre-processing protocols", "Yes", "No", "Yes",
+  "\\tabitem Example: Creating a reproducible data pre-processing protocol", "Yes", "No", "No"
 ) %>% 
   mutate(`Student` = cell_spec(`Student`, format = "latex",
                                background = factor(`Student`,
@@ -31,9 +45,9 @@ my_table <- tribble(
   kable_styling(latex_options = c("hold_position", "repeat_header"), 
                 font_size = 10) %>% 
   row_spec(0, angle = 45) %>% 
-  column_spec(1, width = "15em") %>% 
-  group_rows("Very long sequence name for sequence 1", 1, 3, latex_align = 'l') %>% 
-  group_rows("Sequence 2", 4, 6) 
+  column_spec(1, width = "20em") %>% 
+  group_rows("Very long sequence name for sequence 1", 1, 10, latex_align = 'l') %>% 
+  group_rows("Sequence 2", 11, 19) 
                       
 fileConn <- file("tables/module_tracks.tex")
 writeLines(my_table, fileConn)
