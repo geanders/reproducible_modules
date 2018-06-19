722e0823c0db20dd66590843b7e17f8b0ebb914b
Brooke Anderson <brooke.anderson@colostate.edu>
2018-06-18 20:20:23 -0600

    Create draft of table of module tracks

--- R/table_module_tracks.R
+++ R/table_module_tracks.R
@@ -3,27 +3,28 @@ library(tibble)
 library(kableExtra)
 
 my_table <- tribble(
-  ~ ` `, ~ `Student`, ~`PI`, ~ `Research associate`,
-  "\\tabitem Separating data recording and analysis", "Yes", "No", "No",
-  "\\tabitem Principals and power of structured data formats", "Yes", "Yes", "No",
-  "\\tabitem The 'tidy' data format: an implementation of a structured data format", "Yes", "No", "Yes",
-  "\\tabitem Designing templates for tidy data collection", "Yes", "No", "No",
-  "\\tabitem Example: Creating a template for data collection", "Yes", "No", "No",
-  "\\tabitem Power of using a single 'Project' directory for storing and tracking research project files", "Yes", "No", "Yes",
-  "\\tabitem Creating 'Project' templates", "Yes", "No", "No",
-  "\\tabitem Example: Creating a 'Project' template", "Yes", "Yes", "No",
-  "\\tabitem Harnessing version control for transparent data recording", "Yes", "No", "Yes",
-  "\\tabitem Using git and GitLab to enhance the reproducibility of collaborative research", "Yes", "No", "No",
+  ~ ` `, ~ `Student`, ~`PI`, ~ `Biostatistician`, ~ `Technician`, ~ `Undergrad`,
+  "\\tabitem Separating data recording and analysis", "Yes", "Yes", "Yes", "No", "Yes",
+  "\\tabitem Principals and power of structured data formats", "Yes", "Yes", "No", "No", "Yes",
+  "\\tabitem The 'tidy' data format: an implementation of a structured data format", "Yes", "Yes", "No", "No", "No",
+  "\\tabitem Designing templates for 'tidy' data collection", "Yes", "Yes", "No", "No", "No",
+  "\\tabitem Example: Creating a template for 'tidy' data collection", "Yes", "Yes", "Yes", "No", "No",
+  "\\tabitem Power of using a single structured 'Project' directory for storing and tracking research project files", "Yes", "Yes", "No", "No", "Yes",
+  "\\tabitem Creating 'Project' templates", "Yes", "No", "No", "No", "No",
+  "\\tabitem Example: Creating a 'Project' template", "Yes", "Yes", "Yes", "No", "No",
+  "\\tabitem Harnessing version control for transparent data recording", "Yes", "Yes", "No", "No", "Yes",
+  "\\tabitem Enhance the reproducibility of collaborative research with version control platforms", "Yes", "Yes", "No", "No", "Yes",
+  "\\tabitem Using git and GitLab to implement version control", "Yes", "No", "No", "No", "No",
  
-  "\\tabitem Principals and benefits of scripted pre-processing of experimental data", "Yes", "No", "No",
-  "\\tabitem Introduction to R code scripts", "Yes", "Yes", "No",
-  "\\tabitem Simplify scripted pre-processing through R's 'tidyverse' tools", "Yes", "No", "Yes",
-  "\\tabitem Complex data types in experimental data pre-processing", "Yes", "No", "No",
-  "\\tabitem Complex data types in R and Bioconductor", "Yes", "No", "No",
-  "\\tabitem Example: Converting from complex data types to 'tidy' data formats", "Yes", "No", "No",
-  "\\tabitem Introduction to reproducible data pre-processing protocols", "Yes", "Yes", "No",
-  "\\tabitem Introduction to RMarkdown as a tool for creating reproducible data pre-processing protocols", "Yes", "No", "Yes",
-  "\\tabitem Example: Creating a reproducible data pre-processing protocol", "Yes", "No", "No"
+  "\\tabitem Principals and benefits of scripted pre-processing of experimental data", "Yes", "Yes", "No", "Yes", "No",
+  "\\tabitem Introduction to scripted data pre-processing in R", "Yes", "No", "No", "Yes", "No",
+  "\\tabitem Simplify scripted pre-processing through R's 'tidyverse' tools", "Yes", "No", "No", "Yes", "No",
+  "\\tabitem Complex data types in experimental data pre-processing", "Yes", "Yes", "Yes", "Yes", "No",
+  "\\tabitem Complex data types in R and Bioconductor", "Yes", "No", "Yes", "Yes", "No",
+  "\\tabitem Example: Converting from complex to 'tidy' data formats", "Yes", "Yes", "Yes", "Yes", "No",
+  "\\tabitem Introduction to reproducible data pre-processing protocols", "Yes", "Yes", "No", "Yes", "No",
+  "\\tabitem RMarkdown for creating reproducible data pre-processing protocols", "Yes", "No", "No", "Yes", "No",
+  "\\tabitem Example: Creating a reproducible data pre-processing protocol", "Yes", "Yes", "Yes", "Yes", "No"
 ) %>% 
   mutate(`Student` = cell_spec(`Student`, format = "latex",
                                background = factor(`Student`,
@@ -33,21 +34,35 @@ my_table <- tribble(
                                background = factor(`PI`,
                                                    levels = c("No", "Yes"),
                                                    labels = c("white", "pink"))),
-         `Research associate` = cell_spec(`Research associate`, format = "latex",
-                               background = factor(`Research associate`,
+         `Biostatistician` = cell_spec(`Biostatistician`, format = "latex",
+                               background = factor(`Biostatistician`,
                                                    levels = c("No", "Yes"),
-                                                   labels = c("white", "pink")))) %>% 
-  kable("latex", booktabs = T, escape = FALSE,
+                                                   labels = c("white", "pink"))),
+         `Technician` = cell_spec(`Technician`, format = "latex",
+                               background = factor(`Technician`,
+                                                   levels = c("No", "Yes"),
+                                                   labels = c("white", "pink"))),
+         `Undergrad` = cell_spec(`Undergrad`, format = "latex",
+                                  background = factor(`Undergrad`,
+                                                      levels = c("No", "Yes"),
+                                                      labels = c("white", "pink")))) %>% 
+  kable("latex", booktabs = T, escape = FALSE, 
         align = 'c',
-        col.names = linebreak(c("", "Long description of\nthe student", "Module 2",
-                                "Another very long\ndescription of the\nresearch associate")),
-        caption = "Some examples of tracks of training modules for sample trainees") %>% 
+        col.names = linebreak(c("", 
+                                "\\textbf{Graduate student}\nwho would like to\nlearn in detail\nhow to use\nreproducibility tools\nfor data recording\nand pre-processing\nand is willing to learn\nR programming tools", 
+                                "\\textbf{Principal investigator}\nwho does not program\nbut would like to\nlearn how his/her\nresearch team could\nimprove reproducibility\nof data recording\nand pre-processing",
+                                "\\textbf{Biostatistician}\nwho would\nlike to understand\nbarriers faced by\ncollaborators\nin implementing\nreproducibility\nprincipals early\nin research projects",
+                                "\\textbf{Technician}\nin charge of\nrunning and\npre-processing\nmass\nspectrometry\ndata",
+                                "\\textbf{Undergraduate}\n\\textbf{student}\nwho wants an\nintroduction\nto improving\nreproducibility\nof data\nrecording"),
+                              align = 'c'),
+        caption = "Examples of how different types of trainees might use subsets of the training modules to meet their specific training needs.") %>% 
   kable_styling(latex_options = c("hold_position", "repeat_header"), 
-                font_size = 8) %>% 
-  row_spec(0, angle = 45) %>% 
-  column_spec(1, width = "20em") %>% 
-  group_rows("Very long sequence name for sequence 1", 1, 10, latex_align = 'l') %>% 
-  group_rows("Sequence 2", 11, 19) 
+                font_size = 9) %>% 
+  row_spec(0, align = 'c') %>% 
+  column_spec(1, width = "28em") %>% 
+  group_rows("Improving the Reproducibility of Experimental Data Recording", 1, 11, latex_align = 'l') %>% 
+  group_rows("Improving the Reproducibility of Experimental Data Pre-Processing", 12, 20) %>% 
+  landscape()
                       
 fileConn <- file("tables/module_tracks.tex")
 writeLines(my_table, fileConn)
