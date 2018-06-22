64d6b5b499b4113980843da6a335b90fb74a92e8
Brooke Anderson <brooke.anderson@colostate.edu>
2018-06-22 12:28:44 -0600

    Clean up some of the material on potential piloting / evaluation questions

--- R/table_evaluation.R
+++ R/table_evaluation.R
@@ -15,10 +15,20 @@ my_table <- tribble(
   "\\tabitem How many trainees attended on-campus CSU piloting?", "Yes", "No", "Yes", "No",
   "\\tabitem How many non-CSU pilot testers participated?", "Yes", "No", "Yes", "No",
   "\\tabitem How long do trainees stay on the webpage for the module?", "Yes", "No", "Yes", "No",
+  "\\tabitem For each module video, how often has it been watched?", "Yes", "No", "Yes", "No",
   "\\tabitem When trainees watch a module's video, on average what percent do they watch?", "Yes", "No", "Yes", "No",
-  "\\tabitem Did this module provide the trainee novel information?", "Yes", "No", "Yes", "No",
+  "\\tabitem How often are additional educational materials (quizzes, applied exercise materials) used?", "Yes", "No", "Yes", "No",
+  "\\tabitem How often is the entire book downloaded as a PDF or EPUB file?", "Yes", "No", "Yes", "No",
+  "\\tabitem Which of the modules are used most frequently?", "Yes", "No", "Yes", "No",
+  "\\tabitem What were the trainee's goals in using this training material?", "Yes", "No", "Yes", "No", 
+  "\\tabitem Did this module provide the trainee novel information?", "Yes", "No", "Yes", "No", 
   "\\tabitem Does the trainee plan to change research practices based on having taken the module?", "Yes", "No", "Yes", "No", 
-  "\\tabitem Was the module useful enough that the trainee would recommend it to other scientists?", "Yes", "No", "Yes", "No"
+  "\\tabitem Is so, how does the trainee plan to change research practices based on having taken the module?", "Yes", "No", "Yes", "No", 
+  "\\tabitem Was the module useful enough that the trainee would recommend it to other scientists?", "Yes", "No", "Yes", "No",
+  "\\tabitem Which elements of the training modules (video lecture, written text, additional educational materials) did the trainee find most useful?", "Yes", "No", "Yes", "No",
+  "\\tabitem For each module video, are there spots where it is common for trainees to stop watching?", "Yes", "No", "Yes", "No",
+  "\\tabitem Why did the trainee chose which modules to use?", "Yes", "No", "Yes", "No",
+  "\\tabitem For the modules taken, what content did the trainee wish had been covered but was not?", "Yes", "No", "Yes", "No"
 ) %>% 
   mutate(`CSU pilot testers` = cell_spec(`CSU pilot testers`, format = "latex",
                                background = factor(`CSU pilot testers`,
@@ -47,14 +57,14 @@ my_table <- tribble(
                 font_size = 8) %>% 
   row_spec(0, angle = 45) %>% 
   column_spec(1, width = "30em") %>% 
-  column_spec(2, width = "3em") %>% 
-  column_spec(3, width = "3em") %>% 
-  column_spec(4, width = "3em") %>% 
-  column_spec(5, width = "3em") %>% 
-  group_rows("What are the characteristics of the trainees?", 1, 3, latex_align = 'l') %>% 
-  group_rows("How often are the training materials used?", 4, 9, latex_align = 'l') %>% 
-  group_rows("Patterns in use of each module", 10, 11, latex_align = 'l') %>% 
-  group_rows("Usefulness of each module", 12, 14, latex_align = 'l')
+  column_spec(2, width = "5em") %>% 
+  column_spec(3, width = "5em") %>% 
+  column_spec(4, width = "5em") %>% 
+  column_spec(5, width = "5em") %>% 
+  group_rows("Characteristics of the trainees?", 1, 3, latex_align = 'l') %>% 
+  group_rows("How often the training materials are used", 4, 9, latex_align = 'l') %>% 
+  group_rows("Patterns in use of each module", 10, 15, latex_align = 'l') %>% 
+  group_rows("Usefulness of each module", 16, 23, latex_align = 'l')
 
 fileConn <- file("tables/evaluation.tex")
 writeLines(my_table, fileConn)
