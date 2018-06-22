bcd596cdb878092659d700ca567deb62f6fe7dd9
Brooke Anderson <brooke.anderson@colostate.edu>
2018-06-22 11:04:38 -0600

    Edit our timeline
    This should be pretty ready to go now.

--- R/plot_timeline.R
+++ R/plot_timeline.R
@@ -7,116 +7,186 @@ library(ggthemr)
 
 # Make a dataframe with tasks
 my_df <- tribble(
-  ~ milestones, ~ tasks, ~ startDate, ~ dueDate, ~ status,
+  ~ milestones, ~ tasks, ~ startDate, ~ dueDate, 
   
+## Module development
+
   "Module development", 
-  "Create first draft of text for online book",
+  "Write text for online book",
   ymd("2019-04-01"),
-  ymd("2020-03-31"),
-  "I",
-  
-  "Module development", 
-  "Refine text for online book based on evaluations",
-  ymd("2020-04-01"),
-  ymd("2022-03-31"),
-  "I",
+  ymd("2021-01-01"),
   
   "Module development",
-  "Create content for video lectures",
+  "Create slides for video lectures",
   ymd("2019-04-01"),
-  ymd("2021-03-31"),
-  "I",
+  ymd("2021-01-01"),
   
   "Module development",
-  "Film video lectures / embed in book",
+  "Film video lectures and embed in online book",
   ymd("2019-09-01"),
   ymd("2021-03-31"),
-  "I",
-  
-  "Module piloting, refinement, and evaluation",
-  "Day-length user testing at CSU",
+
+  "Module development",
+  "Format and post online book with\nassistance of student hourly",
+  ymd("2019-04-01"),
+  ymd("2021-03-31"),
+
+  "Module development",
+  "Create additional educational materials (quizzes,\ndiscussion questions, applied exercises)",
+  ymd("2019-09-01"),
+  ymd("2021-03-31"),
+
+  "Module development",
+  "Record audio and video discussions /\nsolutions to applied materials",
+  ymd("2020-01-01"),
+  ymd("2021-03-31"),
+
+  "Module development",
+  "Attend R conference to ensure materials include\nnewest techniques / learn bookdown developments",
+  ymd("2019-07-04"),
+  ymd("2019-07-07"),
+ 
+# Module maintenance and refinement
+
+  "Module maintenance and refinement", 
+  "Refine written text for online book\nbased on pilot feedback",
+  ymd("2020-01-01"),
+  ymd("2022-03-31"),
+
+  "Module maintenance and refinement", 
+  "Ensure training materials comply\nwith Rehabilitation Act",
+  ymd("2019-04-01"),
+  ymd("2022-03-31"),
+
+  "Module maintenance and refinement", 
+  "Refilm any video lecture content if\nneeded based on pilot feedback",
+  ymd("2021-04-01"),
+  ymd("2022-03-31"),
+
+  "Module maintenance and refinement", 
+  "Make any needed refinements to quizzes,\napplied exercises, and discussion questions",
+  ymd("2021-04-01"),
+  ymd("2022-03-31"),
+
+  "Module maintenance and refinement", 
+  "Attend NIGMS Program Meeting",
+  ymd("2021-05-01"),
+  ymd("2021-05-03"),
+
+# Module piloting and evaluation
+
+  "Module piloting and evaluation",
+  "Develop feedback survey for\nCSU pilot testers",
+  ymd("2019-08-01"),
+  ymd("2019-08-31"),
+
+  "Module piloting and evaluation",
+  "Train with CSU STEM Center for strategies to get\nfocus-group-type feedback from CSU pilot session",
+  ymd("2019-08-01"),
+  ymd("2019-08-03"),
+
+  "Module piloting and evaluation",
+  "Develop feedback survey to\ninclude in online book",
+  ymd("2020-08-01"),
+  ymd("2020-08-31"),
+
+  "Module piloting and evaluation",
+  "Day-long pilot testing at CSU",
   ymd("2020-04-01"),
   ymd("2020-04-02"),
-  "I", 
-  
-  "Module piloting, refinement, and evaluation",
-  "Day-length user testing at CSU",
+
+  "Module piloting and evaluation",
+  "Day-long pilot testing at CSU",
   ymd("2019-09-01"),
   ymd("2019-09-02"),
-  "I",  
   
-  "Module piloting, refinement, and evaluation",
-  "Day-length user testing at CSU",
+  "Module piloting and evaluation",
+  "Day-long pilot testing at CSU",
   ymd("2020-04-01"),
   ymd("2020-04-02"),
-  "I", 
   
-  "Module piloting, refinement, and evaluation",
-  "Day-length user testing at CSU",
+  "Module piloting and evaluation",
+  "Day-long pilot testing at CSU",
   ymd("2020-09-01"),
   ymd("2020-09-02"),
-  "I",  
   
-  "Module piloting, refinement, and evaluation",
-  "Day-length user testing at CSU",
+  "Module piloting and evaluation",
+  "Day-long pilot testing at CSU",
   ymd("2021-04-01"),
   ymd("2021-04-02"),
-  "I", 
   
-  "Module piloting, refinement, and evaluation",
-  "Day-length user testing at CSU",
+  "Module piloting and evaluation",
+  "Day-long pilot testing at CSU",
   ymd("2021-09-01"),
   ymd("2021-09-02"),
-  "I", 
   
-  "Module piloting, refinement, and evaluation",
+  "Module piloting and evaluation",
   "Ongoing piloting with non-CSU collaborators",
   ymd("2019-09-01"),
   ymd("2022-03-31"),
-  "I",
+
+  "Module piloting and evaluation",
+  "Ongoing piloting with early online users",
+  ymd("2019-06-01"),
+  ymd("2022-03-31"),
   
-  "Module piloting, refinement, and evaluation",
-  "Workshop at conference to pilot training materials",
-  ymd("2019-09-01"),
-  ymd("2021-03-31"),
-  "I",
+  "Module piloting and evaluation",
+  "Workshop at American Society for Microbiology\nconference to pilot training materials",
+  ymd("2020-06-15"),
+  ymd("2020-06-16"),
+
+# Module dissemination
   
-  "Module dissemination and maintenance",
+  "Module dissemination",
   "Make all materials available through\nonline book as they are developed",
   ymd("2019-04-01"),
   ymd("2022-03-31"),
-  "I",
   
-  "Module dissemination and maintenance",
+  "Module dissemination",
   "Share links through social media to book as it develops",
   ymd("2019-05-15"),
-  ymd("2020-12-31"),
-  "I",
+  ymd("2022-03-31"),
   
-  "Module dissemination and maintenance",
-  "Invite CSU and non-CSU colleagues to \nbe involved in pilot testing materials",
+  "Module dissemination",
+  "Recruit CSU and non-CSU colleagues to\nbe involved in pilot testing materials",
   ymd("2019-08-01"),
-  ymd("2021-06-30"),
-  "I",
+  ymd("2021-09-01"),
   
-  "Module dissemination and maintenance",
+  "Module dissemination",
   "Write and submit paper describing modules",
   ymd("2021-01-01"),
-  ymd("2021-06-16"),
-  "I",
+  ymd("2021-09-16"),
   
-  "Module dissemination and maintenance",
+  "Module dissemination",
   "Present modules at microbiology / immunology conferences",
-  ymd("2020-06-15"),
-  ymd("2020-06-16"),
-  "I",
+  ymd("2020-06-01"),
+  ymd("2020-06-04"),
+
+  "Module dissemination",
+  "Present modules at microbiology / immunology conferences",
+  ymd("2020-06-27"),
+  ymd("2020-06-30"),
   
-  "Module dissemination and maintenance",
+  "Module dissemination",
   "Present modules at microbiology / immunology conferences",
   ymd("2021-06-15"),
   ymd("2021-06-16"),
-  "I"
-) 
+
+  "Module dissemination",
+  "Post static version of the book to bookdown.org",
+  ymd("2021-03-30"),
+  ymd("2021-03-31"),
+
+  "Module dissemination",
+  "Post static version of the book to bookdown.org",
+  ymd("2022-03-30"),
+  ymd("2022-03-31"),
+) %>% 
+  mutate(milestones = fct_relevel(milestones, 
+                                  "Module development",
+                                  "Module maintenance and refinement",
+                                  "Module piloting and evaluation",
+                                  "Module dissemination"))
 
 # Create the figure
 my_timeline <- my_df %>%  
@@ -127,17 +197,19 @@ my_timeline <- my_df %>%
   geom_segment(size = 2) + 
   geom_vline(aes(xintercept = ymd("2019-04-01"))) +
   geom_vline(aes(xintercept = ymd("2022-03-31"))) +
+  geom_vline(aes(xintercept = ymd("2020-03-31")), linetype = 2) +
+  geom_vline(aes(xintercept = ymd("2021-03-31")), linetype = 2) +
   geom_point(aes(x = startDate, 
-                 y = fct_reorder(tasks, startDate, .desc = TRUE))) +
+                 y = fct_reorder(tasks, startDate, fun = min, .desc = TRUE))) +
   geom_point(aes(x = dueDate,
-                 y = fct_reorder(tasks, startDate, .desc = TRUE))) +
-  labs(x = "Date", y = "") + 
+                 y = fct_reorder(tasks, startDate, fun = min, .desc = TRUE))) +
+  labs(x = "", y = "") + 
   theme(legend.position = "none") + 
   scale_color_brewer(palette = "Dark2") + 
   facet_wrap(~ milestones, ncol = 1, scales = "free_y") 
 
 # Write out as pdf
-pdf("figures/timeline.pdf", width = 10, height = 6)
+pdf("figures/timeline.pdf", width = 10, height = 12)
 ggthemr("dust")
 print(my_timeline)
 ggthemr_reset()
